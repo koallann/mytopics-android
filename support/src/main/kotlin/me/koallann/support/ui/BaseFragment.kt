71663f07743a196261c10d4e0485fd3fa52bf458
Allan Lima <allan.lima@ilhasoft.com.br>
2020-08-16 02:55:19 -0300

    Add BaseFragment

--- support/src/main/kotlin/me/koallann/support/ui/BaseFragment.kt
+++ support/src/main/kotlin/me/koallann/support/ui/BaseFragment.kt
@@ -1,18 +1,22 @@
 package me.koallann.support.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.ProgressBar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import me.koallann.support.R
 import me.koallann.support.mvp.BasicView
 
-abstract class BaseActivity : AppCompatActivity(), BasicView {
+abstract class BaseFragment : Fragment(), BasicView {
 
-    private val root: CoordinatorLayout by lazy {
-        findViewById<CoordinatorLayout>(R.id.root)
+    private lateinit var root: CoordinatorLayout
+
+    private val progressBar: ProgressBar by lazy {
+        root.findViewById<ProgressBar>(R.id.progressBar)
     }
     private val content: View by lazy {
         getContentView()
@@ -20,32 +24,27 @@ abstract class BaseActivity : AppCompatActivity(), BasicView {
 
     abstract fun getContentView(): View
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        super.setContentView(R.layout.base)
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        root = inflater.inflate(R.layout.base, container, false) as CoordinatorLayout
         root.addView(content, CoordinatorLayout.LayoutParams(
             CoordinatorLayout.LayoutParams.MATCH_PARENT,
             CoordinatorLayout.LayoutParams.MATCH_PARENT
         ))
-    }
-
-    override fun setContentView(view: View?) {
-        throw IllegalStateException("You must implement the getContentView abstract method!")
-    }
-
-    override fun setContentView(layoutResID: Int) {
-        throw IllegalStateException("You must implement the getContentView abstract method!")
-    }
-
-    override fun setContentView(view: View?, params: ViewGroup.LayoutParams?) {
-        throw IllegalStateException("You must implement the getContentView abstract method!")
+        return root
     }
 
     override fun showLoading() {
+        content.visibility = View.INVISIBLE
+        progressBar.visibility = View.VISIBLE
     }
 
     override fun dismissLoading() {
+        progressBar.visibility = View.INVISIBLE
+        content.visibility = View.VISIBLE
     }
 
     override fun showMessage(message: String) {
