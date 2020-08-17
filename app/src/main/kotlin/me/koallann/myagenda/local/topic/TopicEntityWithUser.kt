3a983e04b5b23cbbd1278203c742261f194f948e
Allan Lima <allan.lima@ilhasoft.com.br>
2020-08-16 22:50:17 -0300

    Fix user join on topic

--- app/src/main/kotlin/me/koallann/myagenda/local/topic/TopicEntityWithUser.kt
+++ app/src/main/kotlin/me/koallann/myagenda/local/topic/TopicEntityWithUser.kt
@@ -1,13 +1,11 @@
 package me.koallann.myagenda.local.topic
 
-import androidx.room.Ignore
 import androidx.room.Relation
 import me.koallann.myagenda.domain.Topic
 import me.koallann.myagenda.local.user.UserEntity
 
 class TopicEntityWithUser : TopicEntity() {
-    @Relation(parentColumn = "id", entityColumn = "user_id", entity = UserEntity::class)
-    @Ignore
+    @Relation(parentColumn = "user_id", entityColumn = "id", entity = UserEntity::class)
     override var user: UserEntity = UserEntity()
 
     fun toDomain(): Topic = Topic(
